401dbee1513a31b45b74c5e3cb79294d23b884ca
MosesChisz <mosesc@cortexware.co.za>
2024-02-21 15:24:01 +0200

    ChatList

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/ChatListItem';
+import ChatScreen from './src/screens/ChatScreen';
 
-const chat = {
-  id: "1",
-  user: {
-    image:
-    "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-    name: "Lukas",
-  },
-  lastMessage: {
-    text: "Oke",
-    createdAt: "07:30",
-  },
-};
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <ChatListItem chat={chat}/>
+      <ChatScreen/>
       
       <StatusBar style="auto" />
     </View>
